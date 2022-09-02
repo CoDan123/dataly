57ea53f8cb3fb3a5134d4e1f2e3110249adb9c5c
CoDan123 <let2350@gmail.com>
2022-09-02 15:23:02 -0600

    added hero bar animation and js comments

--- main.js
+++ main.js
@@ -2,8 +2,15 @@
 const bars = document.getElementById("bars").children;
 
 
-for(let i=0; i < bars.length; i++){
-
+for(let i = 0; i < bars.length; i++){
+    if(i > 0){  //if its not the first element, apply the timeout delay
+        setTimeout(() => {
+            bars[i].classList.toggle("inflate-bars")    //add inflate-bars class to current element in index, which will initiate css animation
+        }, 100 * i);    //the higher the index, the longer the delay
+    } else {
+        bars[i].classList.toggle("inflate-bars")    //if current element is first in loop, apply class without setTimeout/delay
+    }
+    
 }
 
 
