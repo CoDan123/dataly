f3ce8d68664a4c71ed7f4168b722014320f300d2
CoDan123 <let2350@gmail.com>
2022-09-02 18:41:11 -0600

    finished hero bar animation

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-//For Bars SVG Animation Hero Section
-const bars = document.getElementById("bars").children;
 
+const bars = document.getElementById("bars").children;  //For Bars SVG Animation Hero Section
+const dots = document.getElementById("dots").children;  //For Dots SVG Animation Hero Section
+
+for(let i = 0; i < dots.length; i++){
+    if(i > 0){  //if its not the first element, apply the timeout delay
+        setTimeout(() => {
+            dots[i].classList.toggle("inflate-dots")    //add inflate-bars class to current element in index, which will initiate css animation
+        }, 100 * i);    //the higher the index, the longer the delay
+    } else {
+        dots[i].classList.toggle("inflate-dots")    //if current element is first in loop, apply class without setTimeout/delay
+    }
+    
+}
 
 for(let i = 0; i < bars.length; i++){
     if(i > 0){  //if its not the first element, apply the timeout delay
@@ -12,6 +23,7 @@ for(let i = 0; i < bars.length; i++){
     }
     
 }
+
 
 
 
