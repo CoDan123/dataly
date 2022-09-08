97b06fe9058e9dbd1633c66f955afca255651fcd
CoDan123 <let2350@gmail.com>
2022-09-08 10:52:46 -0600

    onload hero fadein animation

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for(let i = 0; i < dots.length; i++){
     if(i > 0){  //if its not the first element, apply the timeout delay
         setTimeout(() => {
             dots[i].classList.toggle("inflate-dots")    //add inflate-bars class to current element in index, which will initiate css animation
-        }, 100 * i);    //the higher the index, the longer the delay
+        }, 120 * i);    //the higher the index, the longer the delay
     } else {
         dots[i].classList.toggle("inflate-dots")    //if current element is first in loop, apply class without setTimeout/delay
     }
@@ -28,7 +28,7 @@ for(let i = 0; i < bars.length; i++){
     if(i > 0){  //if its not the first element, apply the timeout delay
         setTimeout(() => {
             bars[i].classList.toggle("inflate-bars")    //add inflate-bars class to current element in index, which will initiate css animation
-        }, 100 * i);    //the higher the index, the longer the delay
+        }, 120 * i);    //the higher the index, the longer the delay
     } else {
         bars[i].classList.toggle("inflate-bars")    //if current element is first in loop, apply class without setTimeout/delay
     }
